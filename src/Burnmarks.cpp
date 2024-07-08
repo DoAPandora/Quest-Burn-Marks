70602d8211f82ec5105602029345a46666f894bd
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-07-08 19:55:00 -0400

    Fix Burnmarks

--- src/Burnmarks.cpp
+++ src/Burnmarks.cpp
@@ -1,9 +1,8 @@
 #include "beatsaber-hook/shared/utils/logging.hpp"
 #include "beatsaber-hook/shared/utils/hooking.hpp"
-#include "hooks.hpp"
 #include "config.hpp"
 
-#include "GlobalNamespace/SaberBurnMarkArea.hpp"
+//#include "GlobalNamespace/SaberBurnMarkArea.hpp"
 #include "GlobalNamespace/BoolSO.hpp"
 #include "UnityEngine/Camera.hpp"
 #include "UnityEngine/LineRenderer.hpp"
@@ -11,11 +10,11 @@
 #include "UnityEngine/Texture2D.hpp"
 #include "UnityEngine/Material.hpp"
 
-	self->burnMarkTrailsEnabled->set_value(true);
-
+	bool enableBurnMarks = true;
+{
 //stops the marks from spawning, this way it allows it to be configurable during runtime without a reload.
-	if (config.enableBurnMarks) {
-		SaberBurnMarkArea_LateUpdate(self);
-	} else {
+	//if (config.enableBurnMarks) {
+		//SaberBurnMarkArea_LateUpdate(self);
+	//} else {
 		self->_renderer->get_sharedMaterial()->set_mainTexture(UnityEngine::Texture2D::get_blackTexture());
     }
