f62d5e9b6a892419c9bc13f40f15b105c7f89b69
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-07-08 18:54:26 -0400

    UI

---
+++ include/UI/BurnMarksFlowCoordinator.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "custom-types/shared/macros.hpp"
+#include "custom-types/shared/coroutine.hpp"
+#include "HMUI/FlowCoordinator.hpp"
+#include "HMUI/ViewController.hpp"
+
+DECLARE_CLASS_CODEGEN(BurnMarks::UI, BurnMarksFlowCoordinator, HMUI::FlowCoordinator,
+	DECLARE_OVERRIDE_METHOD_MATCH(void, DidActivate, &HMUI::FlowCoordinator::DidActivate, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
+	DECLARE_OVERRIDE_METHOD_MATCH(void, BackButtonWasPressed, &HMUI::FlowCoordinator::BackButtonWasPressed, HMUI::ViewController* topViewController);
+	DECLARE_INSTANCE_FIELD(HMUI::ViewController*, BurnMarksSettingsViewController);
+)
---
+++ include/UI/BurnMarksSettingsViewController.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "custom-types/shared/macros.hpp"
+#include "custom-types/shared/coroutine.hpp"
+#include "HMUI/ViewController.hpp"
+#include "UnityEngine/UI/Toggle.hpp"
+#include "bsml/shared/BSML/Components/Settings/ToggleSetting.hpp"
+
+DECLARE_CLASS_CODEGEN(BurnMarks::UI, BurnMarksSettingsViewController, HMUI::ViewController,
+	DECLARE_INSTANCE_FIELD(BSML::ToggleSetting*, customColourToggle);
+	DECLARE_OVERRIDE_METHOD_MATCH(void, DidActivate, &HMUI::ViewController::DidActivate, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
+	DECLARE_DEFAULT_CTOR();
+)
