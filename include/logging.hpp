99a7e52e3401eb5362609cee0409858b9a15ef02
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-07-08 19:24:46 -0400

    Update logging

--- include/logging.hpp
+++ include/logging.hpp
@@ -1,16 +1,17 @@
 #pragma once
-#include "beatsaber-hook/shared/utils/logging.hpp"
-#include <string_view>
 
-namespace BurnMarks
-{
-    class Logging 
-    {
-        public:
-            static Logger& getLogger();
-            static LoggerContextObject& getContextLogger(const char* fun, const char* file, int line);
-    };
+#include "beatsaber-hook/shared/utils/utils.h"
+#include "paper/shared/logger.hpp"
+
+//#define LOG_INFO(value...)
+#define INFO(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::INF>(str, "BurnMarks" __VA_OPT__(, __VA_ARGS__))
+//#define LOG_DEBUG(value...)
+#define DEBUG(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::DBG>(str, "BurnMarks" __VA_OPT__(, __VA_ARGS__))
+//#define LOG_ERROR(value...)
+#define ERROR(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::ERR>(str, "BurnMarks" __VA_OPT__(, __VA_ARGS__))
+#define WARNING(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::WRN>(str, "BurnMarks" __VA_OPT__(, __VA_ARGS__))
+#define CRITICAL(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::CRIT>(str, "BurnMarks" __VA_OPT__(, __VA_ARGS__))
+
+namespace BurnMarks {
+    static constexpr auto Logger = Paper::ConstLoggerContext("BurnMarks");
 }
-#define INFO(...) ::BurnMarks::Logging::getContextLogger(__PRETTY_FUNCTION__, __FILE__, __LINE__).info(__VA_ARGS__)
-#define ERROR(...) ::BurnMarks::Logging::getContextLogger(__PRETTY_FUNCTION__, __FILE__, __LINE__).error(__VA_ARGS__)
-#define CRITICAL(...) ::BurnMarks::Logging::getContextLogger(__PRETTY_FUNCTION__, __FILE__, __LINE__).critical(__VA_ARGS__)
