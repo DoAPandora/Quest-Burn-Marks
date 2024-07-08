7a29a5b0628b2ce349d0b351abd002b1b0a03df7
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-07-08 19:22:05 -0400

    Update Burnmarks

--- src/Burnmarks.cpp
+++ src/Burnmarks.cpp
@@ -10,15 +10,12 @@
 #include "UnityEngine/Renderer.hpp"
 #include "UnityEngine/Texture2D.hpp"
 #include "UnityEngine/Material.hpp"
-{
+
 	self->burnMarkTrailsEnabled->set_value(true);
-}
 
 //stops the marks from spawning, this way it allows it to be configurable during runtime without a reload.
-MAKE_AUTO_HOOK_MATCH(SaberBurnMarkArea_LateUpdate, &GlobalNamespace::SaberBurnMarkArea::LateUpdate, void, GlobalNamespace::SaberBurnMarkArea* self) {
 	if (config.enableBurnMarks) {
 		SaberBurnMarkArea_LateUpdate(self);
 	} else {
 		self->_renderer->get_sharedMaterial()->set_mainTexture(UnityEngine::Texture2D::get_blackTexture());
     }
-}
