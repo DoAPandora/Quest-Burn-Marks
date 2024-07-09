7465cca4af4ebb4c5178ae8fb20b96ad6f6d1c2f
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-07-08 20:19:46 -0400

    Update Burnmarks

--- src/Burnmarks.cpp
+++ src/Burnmarks.cpp
@@ -2,19 +2,11 @@
 #include "beatsaber-hook/shared/utils/hooking.hpp"
 #include "config.hpp"
 
-//#include "GlobalNamespace/SaberBurnMarkArea.hpp"
-#include "GlobalNamespace/BoolSO.hpp"
-#include "UnityEngine/Camera.hpp"
-#include "UnityEngine/LineRenderer.hpp"
-#include "UnityEngine/Renderer.hpp"
-#include "UnityEngine/Texture2D.hpp"
-#include "UnityEngine/Material.hpp"
-
-	bool enableBurnMarks = true;
+	bool enableBurnMarks = true; {
 
 //stops the marks from spawning, this way it allows it to be configurable during runtime without a reload.
 	//if (config.enableBurnMarks) {
 		//SaberBurnMarkArea_LateUpdate(self);
 	//} else
-		self->_renderer->get_sharedMaterial()->set_mainTexture(UnityEngine::Texture2D::get_blackTexture()); {
+		//self->_renderer->get_sharedMaterial()->set_mainTexture(UnityEngine::Texture2D::get_blackTexture()); {
 }
