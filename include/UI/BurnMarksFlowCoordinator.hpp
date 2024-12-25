2e1774641404d3f092d5ffb2ecb86c513122b341
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-12-25 16:55:20 -0500

    Update UI

--- include/UI/BurnMarksFlowCoordinator.hpp
+++ include/UI/BurnMarksFlowCoordinator.hpp
@@ -10,16 +10,16 @@
 #define GET_FIND_METHOD(mPtr) il2cpp_utils::il2cpp_type_check::MetadataGetter<mPtr>::get()
 
 DECLARE_CLASS_CODEGEN(BurnMarks::UI, BurnMarksFlowCoordinator, HMUI::FlowCoordinator,
-    DECLARE_INSTANCE_FIELD(BurnMarks::UI::SettingsView*, settingsView);
+    //DECLARE_INSTANCE_FIELD(BurnMarks::UI::SettingsView*, settingsView);
 
-    DECLARE_INSTANCE_METHOD(void, Awake);
+    //DECLARE_INSTANCE_METHOD(void, Awake);
 
     DECLARE_OVERRIDE_METHOD_MATCH(void, DidActivate, &HMUI::FlowCoordinator::DidActivate, bool firstActivation, bool addedToHeirarchy, bool screenSystemEnabling);
-    DECLARE_OVERRIDE_METHOD_MATCH(void, BackButtonWasPressed, &HMUI::FlowCoordinator::BackButtonWasPressed, HMUI::ViewController* topViewController);
+    //DECLARE_OVERRIDE_METHOD_MATCH(void, BackButtonWasPressed, &HMUI::FlowCoordinator::BackButtonWasPressed, HMUI::ViewController* topViewController);
 
 )
 
     //void Close(bool immediately = false);
 
 
-//inline SafePtrUnity<BurnMarks::UI::BurnMarksFlowCoordinator> fcInstance; // Stores the ID and version of the mod, and is sent to the modloader upon startup
+inline SafePtrUnity<BurnMarks::UI::BurnMarksFlowCoordinator> fcInstance; // Stores the ID and version of the mod, and is sent to the modloader upon startup
