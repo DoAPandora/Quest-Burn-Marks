513f1a46da1cc56d1a810027b59d3c88227f6e00
DoA Voices <100880439+DoAPandora@users.noreply.github.com>
2024-12-25 16:56:53 -0500

    Update Burnmarks

--- src/Burnmarks.cpp
+++ src/Burnmarks.cpp
@@ -5,7 +5,7 @@
 	bool enableBurnMarks = true;
 
 //stops the marks from spawning, this way it allows it to be configurable during runtime without a reload.
-	//if (config.enableBurnMarks) {
-		//SaberBurnMarkArea_LateUpdate(self);
-	//} else
+	if (config.enableBurnMarks) {
+		SaberBurnMarkArea_LateUpdate(self);
+	}
 		//self->_renderer->get_sharedMaterial()->set_mainTexture(UnityEngine::Texture2D::get_blackTexture()); {
